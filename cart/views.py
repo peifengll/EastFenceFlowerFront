d344c80331823d36bc2058365bc8a360d02420dc
peifengll <peifengll@qq.com>
2024-01-23 23:06:21 +0800

    refactor: :recycle: 修改增加购物车收藏功能
    获取前端的数据使用request.body

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 
@@ -10,60 +11,62 @@ from models.models import Cart
 
 # Create your views here.
 
+
 class CartAddView(APIView):
+
     def post(self, request):
-        userid = request.POST.get('user_id')
-        print(userid)
+        # print("json", request.body)
+        request_data = request.body.decode("utf-8")
+        # print("data : ", request_data)
+        try:
+            request_data = json.loads(request_data)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON format"})
+        # print("request_data: ", request_data)
+        # 提取参数
+        gname = request_data.get('gname')
+        # print("gname: ", gname)
+        # 解析 goods_ids 字符串为列表
+        goods_ids = request_data.get('goods_id', '[]')
+        # print("goods_ids_str: ",goods_ids_str[0])
+        # goods_ids = json.loads(goods_ids_str)
+        # print("goods_ids loads: ", goods_ids)
+        num = request_data.get('num')
+        uname = request_data.get('uname')
+        userid = request_data.get('user_id')
+
+        # 解析 price 字符串为字典
+        price = request_data.get('price', '{}')
+        # print("price_str", price)
+        # print(price[goods_ids[0]])
+        # price = json.loads(price_str)
         if userid is None or userid == "":
             return BaseResponse(data={}, status=401, msg="用户凭证丢失")
-        uname = request.POST.get('uname')
-        gname = request.POST.get('gname')
-        goodsid = request.POST.get('goods_id')
-        try:
-            goodsid = json.loads(goodsid)
-        except Exception as e:
-            return BaseResponse(msg="数据解析错误", status=500)
 
-        price = request.POST.get('price')
-        try:
-            price = json.loads(price)
-        except Exception as e:
-            return BaseResponse(msg="数据解析错误", status=500)
-
-        num = request.POST.get('num')
-        if goodsid == "" or goodsid is None:
+        if goods_ids == "" or goods_ids is None:
             return BaseResponse(data={}, status=301, msg="商品消息丢失")
-        try:
-            k = Cart.objects.create(user_id=userid,
-                                    uname=uname,
-                                    gname=gname,
-                                    goods_id=goodsid[0],
-                                    num=num,
-                                    price=price[goodsid[0]]
-                                    )
+        for i in goods_ids:
+            # 如果是同样的数据，
+            c = None
+            try:
+                c = Cart.objects.get(user_id=userid, goods_id=i)
+            except Exception as e:
+                pass
+            try:
+                if c is not None:
+                    Cart.objects.filter(user_id=userid, goods_id=i).update(num=int(c.num) + int(num))
+                else:
+                    k = Cart.objects.create(user_id=userid,
+                                            uname=uname,
+                                            gname=gname,
+                                            goods_id=i,
+                                            num=num,
+                                            price=price[i]
+                                            )
 
-            print(k)
-            k = Cart.objects.create(user_id=userid,
-                                    uname=uname,
-                                    gname=gname,
-                                    goods_id=goodsid[1],
-                                    num=num,
-                                    price=price[goodsid[1]]
-                                    )
-
-            print(k)
-            k = Cart.objects.create(user_id=userid,
-                                    uname=uname,
-                                    gname=gname,
-                                    goods_id=goodsid[2],
-                                    num=num,
-                                    price=price[goodsid[2]]
-                                    )
-
-            print(k)
-        except Exception as e:
-            print(e)
-            return BaseResponse(data={'error': e.__str__()}, status=500, msg="添加失败")
+                    print(k)
+            except Exception as e:
+                return BaseResponse(data={'error': e.__str__()}, status=500, msg="添加失败")
         return BaseResponse(data={}, status=200, msg="添加成功")
 
 
